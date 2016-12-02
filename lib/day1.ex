344c5d973020779d601462610756073a1647c74b
Keith Salisbury <keithsalisbury@gmail.com>
2016-12-02 16:48:52 +0000

    day 1 rough

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,4 +1,35 @@
 defmodule Day1 do
-  def move({x,y},"R" <> count), do: {x+String.to_integer(count),y}
-  def move({x,y},"L" <> count), do: {x-String.to_integer(count),y}
+  @start_position {0,0,:n}
+
+  def shortest_path(instructions) do
+    {x,y,_} = move(@start_position, instructions)
+    taxicab_distance({0,0}, {x,y})
+  end
+
+  def taxicab_distance({x1,y1},{x2,y2}), do: Kernel.abs(x1-x2) + Kernel.abs(y1-y2)
+
+  def move(pos, instructions) do
+    parse(pos, String.split(instructions, ", "))
+  end
+
+  defp parse(pos, []), do: pos
+  defp parse(pos, [hd|tail]) do
+    pos = position(pos, hd)
+    parse(pos, tail)
+  end
+
+  defp position(pos, instruction, count \\ nil)
+  defp position(pos, instruction, nil) do
+    <<dir::utf8>> <> count = instruction
+    position(pos, dir, String.to_integer(count))
+  end
+  # L = 76 && R == 82
+  defp position({x,y,:n}, 82, count), do: {x+count,y,:e}
+  defp position({x,y,:n}, 76, count), do: {x-count,y,:w}
+  defp position({x,y,:e}, 82, count), do: {x,y-count,:s}
+  defp position({x,y,:e}, 76, count), do: {x,y+count,:n}
+  defp position({x,y,:s}, 82, count), do: {x-count,y,:w}
+  defp position({x,y,:s}, 76, count), do: {x+count,y,:e}
+  defp position({x,y,:w}, 82, count), do: {x,y+count,:n}
+  defp position({x,y,:w}, 76, count), do: {x,y-count,:s}
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -3,18 +3,54 @@ defmodule Day1Test do
   doctest Day1
 
   test "move R1" do
-    assert Day1.move({0,0}, "R1") == {1,0}
+    assert Day1.move({0,0,:n}, "R1") == {1,0,:e}
   end
 
   test "move R2" do
-    assert Day1.move({0,0}, "R2") == {2,0}
+    assert Day1.move({0,0,:n}, "R2") == {2,0,:e}
   end
 
   test "move L1" do
-    assert Day1.move({0,0}, "L1") == {-1,0}
+    assert Day1.move({0,0,:n}, "L1") == {-1,0,:w}
   end
 
   test "move L2" do
-    assert Day1.move({0,0}, "L2") == {-2,0}
+    assert Day1.move({0,0,:n}, "L2") == {-2,0,:w}
+  end
+
+  test "from east move R1" do
+    assert Day1.move({0,0,:e}, "R1") == {0,-1,:s}
+  end
+
+  test "move R1, L1" do
+    assert Day1.move({0,0,:n}, "R1, L1") == {1,1,:n}
+  end
+
+  test "move R1, L1, R1, L1" do
+    assert Day1.move({0,0,:n}, "R1, L1, R1, L1") == {2,2,:n}
+  end
+
+  test "move R1, R1, R1, R1" do
+    assert Day1.move({0,0,:n}, "R1, R1, R1, R1") == {0,0,:n}
+  end
+
+  test "move R2, L3" do
+    assert Day1.move({0,0,:n}, "R2, L3") == {2,3,:n}
+  end
+
+  test "0,0 to 2,3" do
+    assert Day1.taxicab_distance({0,0}, {2,3}) == 5
+  end
+
+  test "R2, L3" do
+    assert Day1.shortest_path("R2, L3") == 5
+  end
+
+  test "R2, R2, R2" do
+    assert Day1.shortest_path("R2, R2, R2") == 2
+  end
+
+  test "R5, L5, R5, R3" do
+    assert Day1.shortest_path("R5, L5, R5, R3") == 12
   end
 end
